d3de88bf3b61a290c5fecb64d93233d087a39e30
anmol02singh <anmol_singh02@outlook.com>
2024-11-07 16:13:00 -0500

    added cors policy for requests, might remove later

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const express = require('express');
 const app = express();
 const connectToDatabase = require('./config/database');
 const routes = require('./routes/routes');
+const cors = require('cors');
 
 connectToDatabase();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/cdm-data', routes);
 
