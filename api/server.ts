f6a55cf38834db70f693fd3a43dd90f8f720a045
Taha Shahid <90358100+tahas02@users.noreply.github.com>
2025-04-25 00:39:16 -0400

    Working on deployment

---
+++ api/server.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import connectToDatabase from "../config/database";
+import cdmRoutes from "../routes/cdmRoutes";
+import otherRoutes from "../routes/otherRoutes";
+
+const app = express();
+
+connectToDatabase();
+
+app.use(express.json({ limit: "150kb" }));
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ limit: "150kb", extended: true }));
+
+app.use("/cdm-data", cdmRoutes);
+app.use("/", otherRoutes);
+
+app.listen(3000, () => console.log("Server Started"));
